5e9b4dcc79b00e65723d671f6c4a939c36bc5eea
Matthew Farrugia-Roberts <matt@far.in.net>
2020-12-02 15:36:03 +1100

    drill verbs

--- de.verb.mg/graph.py
+++ de.verb.mg/graph.py
@@ -1,8 +1,11 @@
 def graph():
     for line in LINKS.splitlines()[1:]:
+        line = line.split("#", maxsplit=1)[0]
+        if "--" not in line:
+            continue
         de, en = line.split("--")
         de = de.strip()
-        en = en.split("#")[0].strip()
+        en = en.strip()
         yield ("de.verb", en, de)
 
 LINKS = """de -- en                                     # notes (ignored)
@@ -21,7 +24,7 @@ machen        -- to make/do
 geben         -- to give
 kommen        -- to come
 gehen         -- to go
-wissen        -- to know
+wissen        -- to know (1)
 sehen         -- to see
 lassen (1)    -- to let/allow
 lassen (2)    -- to have done
@@ -46,7 +49,7 @@ leben         -- to live (alive)
 fahren        -- to go/ride/drive/etc.
 meinen        -- to think/opine
 fragen        -- to ask
-kennen        -- to know
+kennen        -- to know (2)
 gelten        -- to be valid (not expired)
 stellen       -- to place/set
 spielen       -- to play
@@ -120,4 +123,66 @@ fehlen        -- to lack
 bedeuten      -- to mean/represent
 vergleichen   -- to compare
 möchten       -- to would like                          # modal, K2P of mögen
+
+# verbs gathered randomly e.g. from class, but i remember them anyway
+atmen         -- to breath
+sterben       -- to die
+hören         -- to hear
+lachen        -- to laugh
+lieben        -- to love
+hassen        -- to hate
+essen         -- to eat
+trinken       -- to drink
+fressen       -- to feed
+rennen (1)    -- to run/race
+rennen (2)    -- to scramble
+vergessen     -- to forget
+kaufen        -- to buy
+einkaufen     -- to shop                                # ein + kaufen
+verkaufen     -- to sell                                # ver + kaufen
+passen (1)    -- to fit/suit
+passen (2)    -- to match
+kosten        -- to cost
+stehlen       -- to steal
+zahlen        -- to pay (1)                             # difference?
+bezahlen      -- to pay (2)                             # be + zahlen
+bitten        -- to thank
+gefallen      -- to please/appeal to
+schlaffen     -- to sleep
+kochen        -- to cook
+backen        -- to bake
+besuchen      -- to visit                               # be + suchen?
+schneien      -- to snow
+regnen        -- to rain
+üben          -- to practice
+antworten     -- to answer
+diskutieren   -- to discuss
+analysieren   -- to analyse
+buchstabieren -- to spell
+streichen (1) -- to paint/smear
+streichen (2) -- to eliminate
+streichen (3) -- to decorate
+unterstreichen -- to underline                          # unter + streichen
+rechnen       -- to calculate
+notieren      -- to note (down)
+markieren     -- to highlight
+ergänzen (1)  -- to complete/enter/fill in              # er + gänzen?
+ergänzen (2)  -- to complete/complement
+ordnen        -- to order/arrange
+zuordnen      -- to collate                             # zu + ordnen
+lösen         -- to solve
+zeichnen      -- to draw
+schneiden     -- to cut
+öffnen        -- to open
+messen        -- to measure
+fangen        -- to catch
+leihen        -- to loan/borrow
+treten        -- to step
+brechen       -- to break
+unterbrechen  -- to interrupt                           # unter + brechen
+befehlen      -- to command                             # emp + fehlen?
+empfehlen     -- to recommend                           # emp + fehlen?
+werfen        -- to throw
+holen         -- to fetch/get
+wiederholen   -- to repeat                              # wieder + holen?
 """
